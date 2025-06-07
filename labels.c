b47782df8b46fb117e263231bc11d07306bbf9f2
Pedrex25 <pedrobernardo7192@gmail.com>
2025-06-07 03:31:58 +0100

    Initial Commit

---
+++ labels.c
@@ -0,0 +1,156 @@
+void logo()
+{
+    printf("⠀⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⢤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⢀⡾⠁⠀⣀⠔⠁⠀⠀⠈⠙⠷⣤⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⣠⠞⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠈⢿⡀⢠⡶⠒⠳⠶⣄⠀⠀⠀⠀⠀⣴⠟⠁⠀⠀⠀⣰⠏⠀⢀⣤⣤⣄⡀⠀⠀\n");
+    printf("⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠛⠛⠃⠸⡇⠈⣇⠸⡇⠀⠀⠀⠘⣇⠀⠀⣠⡾⠁⠀⠀⠀⢀⣾⣣⡴⠚⠉⠀⠀⠈⠹⡆⠀\n");
+    printf("⣹⡷⠤⠤⠤⠄⠀⠀⠀⠀⢠⣤⡤⠶⠖⠛⠀⣿⠀⣿⠀⢻⡄⠀⠀⠀⢻⣠⡾⠋⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⢀⣠⡾⠃⠀\n");
+    printf("⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏⠀⠀⣿⠀⠀⠀⠘⠉⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠀⠀\n");
+    printf("⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⠀⢀⣴⡿⠥⠶⠖⠛⠛⢶⡄\n");
+    printf("⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀⠀⠀⠀⢀⣰⡇⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣠⠼⠃\n");
+    printf("⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀⠀⠀⠀⠀⠀⣿⠉⣇⠀⡴⠟⠁⣠⡾⠃⠀⠀⠀⠀⠀⠈⠀⠀⠀⣀⣤⠶⠛⠉⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀⠀⠀⠀⢻⡄⠹⣦⠀⠶⠛⢁⣠⡴⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀⠀⠀⠻⣄⠈⢷⣄⠈⠉⠁⠀⠀⠀⢀⣠⡴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+}
+
+void show_rock()
+{
+    printf("    _______\n");
+    printf("---'   ____)\n");
+    printf("      (_____)\n");
+    printf("      (_____)\n");
+    printf("      (____)\n");
+    printf("---.__(___)\n");
+}
+
+void show_paper()
+{
+    printf("     _______\n");
+    printf("---'   ____)____\n");
+    printf("          ______)\n");
+    printf("         _______)\n");
+    printf("        _______)\n");
+    printf("---.__________)\n");
+}
+
+void show_scissors()
+{
+    printf("    _______\n");
+    printf("---'   ____)____\n");
+    printf("          ______)\n");
+    printf("       __________)\n");
+    printf("      (____)\n");
+    printf("---.__(___)\n");
+}
+
+void show_rock_winner()
+{
+    printf("    _______                 ___________\n");
+    printf("---'   ____)              '._==_==_=_.''\n");
+    printf("      (_____)              .-\\:      /-.\n");
+    printf("      (_____)             | (|:.     |) |\n");
+    printf("      (____)               '-|:.     |-'\n");
+    printf("---.__(___)                  \\::.    /\n");
+    printf("                              '::. .'\n");
+    printf("                                ) (\n");
+    printf("                              _.' '._\n");
+    printf("                            `\"\"\"\"\"\"\"\"`\n");
+}
+
+void show_paper_winner()
+{
+    printf("     _______                  ___________\n");
+    printf("---'   ____)____           '._==_==_=_.''\n");
+    printf("          ______)            .-\\:      /-.\n");
+    printf("         _______)           | (|:.     |) |\n");
+    printf("        _______)             '-|:.     |-'\n");
+    printf("---.__________)                \\::.    /\n");
+    printf("                                '::. .'\n");
+    printf("                                  ) (\n");
+    printf("                                _.' '._\n");
+    printf("                              `\"\"\"\"\"\"\"\"`\n");
+}
+
+void show_scissors_winner()
+{
+    printf("    _______                   ___________\n");
+    printf("---'   ____)____           '._==_==_=_.''\n");
+    printf("          ______)            .-\\:      /-.\n");
+    printf("       __________)          | (|:.     |) |\n");
+    printf("      (____)                 '-|:.     |-'\n");
+    printf("---.__(___)                    \\::.    /\n");
+    printf("                                '::. .'\n");
+    printf("                                  ) (\n");
+    printf("                                _.' '._\n");
+    printf("                              `\"\"\"\"\"\"\"\"`\n");
+}
+
+void show_all_hands()
+{
+    printf("    _______\n");
+    printf("---'   ____)\n");
+    printf("      (_____) \n");
+    printf("      (_____) - [1] Rock\n");
+    printf("      (____)\n");
+    printf("---.__(___)\n");
+
+    printf("     _______\n");
+    printf("---'   ____)____\n");
+    printf("          ______)\n");
+    printf("         _______) - [2] Paper\n");
+    printf("        _______)\n");
+    printf("---.__________)\n");
+
+    printf("    _______\n");
+    printf("---'   ____)____\n");
+    printf("          ______)\n");
+    printf("       __________) - [3] Scissor\n");
+    printf("      (____)\n");
+    printf("---.__(___)\n");
+}
+
+char CPU[] = "CPU";
+char PLAYER[] = "Player";
+
+char CPU_PLAY[] = "CPU:\n";
+char PLAYER_PLAY[] = "Player:\n";
+
+char CPU_WINS_ROUND[] = "CPU wins this round!\n";
+char PLAYER_WINS_ROUND[] = "Player wins this round!\n";
+
+char WELCOME_MSG[] = "Welcome to the Rock-Paper-Scissors game!\n";
+char GAME_DURATION_OPTIONS_MSG[] = "Rounds Available:\n\t[1]- 3 rounds\n\t[2]- 6 rounds\n\t[3]- 9 Rounds\n\n";
+char GAME_DURATION_SELECTION_MSG[] = "Please select the duration of the game:\t";
+char OPTION_NOT_AVAILABLE_MSG[] = "Option not available, please select an option between [1-3]\n";
+
+char PLAYER_TURN_MSG[] = "Player turn:\t";
+
+char ONLY_NUMBERS_ALLOWED_MSG[] = "\nOnly numbers are allowed [1-3]\n";
+
+char NO_WINNER_ROUND_MSG[] = "No winner this round!\n";
+char NO_WINNER_GAME_MSG[] = "No winner this game, its a draw!\n";
+
+void show_winner_score(char winner[], short *score)
+{
+    printf("%s wins the game with %hd points!\n", winner, *score);
+}
+
+void track_rounds(short *remaining_rounds, short *total_rounds)
+{
+    printf("Round: %hd/%hd\n", *remaining_rounds, *total_rounds);
+}
+
+void show_score(short *cpu_score, short *player_score)
+{
+    printf("----------------Score----------------\n-Player score: %hd\n-CPU score: %hd", *player_score, *cpu_score);
+}
+
+char LINE_SEPARATOR[] = "------------------------------------------------------\n";
---
+++ main.c
@@ -0,0 +1,240 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include "labels.c"
+
+enum hands
+{
+    ROCK = 1,
+    PAPER = 2,
+    SCISSORS = 3
+};
+
+enum rounds
+{
+    THREE_ROUNDS = 1,
+    SIX_ROUNDS = 2,
+    NINE_ROUNDS = 3
+};
+
+int main()
+{
+
+    unsigned short flag = 1;
+
+    unsigned short total_rounds = 3;
+    unsigned short choosen_rounds = 1;
+
+    unsigned short min_rand_value = 1;
+    unsigned short max_rand_value = 3;
+
+    unsigned short player_hand;
+    unsigned short player_score = 0;
+    unsigned short cpu_score = 0;
+    // short dificulty = 1;
+
+    srand(time(NULL));
+
+    logo();
+    printf(WELCOME_MSG);
+    printf(GAME_DURATION_OPTIONS_MSG);
+
+    // printf("Game difficulties:\n\t[1]-Easy\n\t[2]-Medium\n\t[3]-Hard");
+
+    while (flag == 1)
+    {
+        short inner_flag = 1;
+
+        printf(GAME_DURATION_SELECTION_MSG);
+        scanf("%hd", &choosen_rounds);
+
+        if (choosen_rounds == THREE_ROUNDS)
+        {
+            total_rounds = 3;
+        }
+        else if (choosen_rounds == SIX_ROUNDS)
+        {
+            total_rounds = 6;
+        }
+
+        else if (choosen_rounds == NINE_ROUNDS)
+        {
+            total_rounds = 9;
+        }
+        else
+        {
+            printf(OPTION_NOT_AVAILABLE_MSG);
+            continue;
+        }
+
+        short remaining_rounds = total_rounds;
+
+        while (inner_flag == 1)
+        {
+            track_rounds(&remaining_rounds, &total_rounds);
+
+            short name_not_valid = 1;
+
+            int cpu_hand = min_rand_value + rand() % (max_rand_value - min_rand_value + 1); // Get a random value between 1-3
+
+            show_all_hands();
+
+            printf(PLAYER_TURN_MSG);
+
+            // Restrict input to numbers only
+            if (scanf("%hd", &player_hand) != 1)
+            {
+                printf(ONLY_NUMBERS_ALLOWED_MSG);
+                continue;
+            }
+
+            // No winner cases
+            if (player_hand == ROCK && cpu_hand == ROCK)
+            {
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_rock();
+                show_rock();
+
+                printf(NO_WINNER_ROUND_MSG);
+                printf(CPU_PLAY);
+                printf(LINE_SEPARATOR);
+            }
+            else if (player_hand == PAPER && cpu_hand == PAPER)
+            {
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_paper();
+                show_paper();
+
+                printf(CPU_PLAY);
+                printf(NO_WINNER_ROUND_MSG);
+                printf(LINE_SEPARATOR);
+            }
+            else if (player_hand == SCISSORS && cpu_hand == SCISSORS)
+            {
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_scissors();
+                show_scissors();
+
+                printf(CPU_PLAY);
+                printf(NO_WINNER_ROUND_MSG);
+                printf(LINE_SEPARATOR);
+            }
+
+            // Player winning cases
+            else if (player_hand == ROCK && cpu_hand == SCISSORS)
+            {
+
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_rock_winner(PLAYER);
+                show_scissors();
+
+                printf(CPU_PLAY);
+                printf(PLAYER_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                player_score++;
+            }
+            else if (player_hand == PAPER && cpu_hand == ROCK)
+            {
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_paper_winner(PLAYER);
+                show_rock();
+
+                printf(CPU_PLAY);
+                printf(PLAYER_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                player_score++;
+            }
+            else if (player_hand == SCISSORS && cpu_hand == PAPER)
+            {
+
+                printf(LINE_SEPARATOR);
+                printf(PLAYER_PLAY);
+
+                show_scissors_winner(PLAYER);
+                show_paper();
+
+                printf(CPU_PLAY);
+                printf(PLAYER_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                player_score++;
+            }
+
+            // Cpu winning cases
+            else if (cpu_hand == ROCK && player_hand == SCISSORS)
+            {
+                printf(LINE_SEPARATOR);
+                printf(CPU_PLAY);
+
+                show_rock_winner(CPU);
+                show_scissors();
+
+                printf(PLAYER_PLAY);
+                printf(CPU_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                cpu_score++;
+            }
+            else if (cpu_hand == PAPER && player_hand == ROCK)
+            {
+                printf(LINE_SEPARATOR);
+                printf(CPU_PLAY);
+
+                show_paper_winner(CPU);
+                show_rock();
+
+                printf(PLAYER_PLAY);
+                printf(CPU_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                cpu_score++;
+            }
+            else if (cpu_hand == SCISSORS && player_hand == PAPER)
+            {
+                printf(LINE_SEPARATOR);
+                printf(CPU_PLAY);
+
+                show_scissors_winner(CPU);
+                show_paper();
+
+                printf(PLAYER_PLAY);
+                printf(CPU_WINS_ROUND);
+                printf(LINE_SEPARATOR);
+                cpu_score++;
+            }
+
+            remaining_rounds--;
+
+            if (remaining_rounds == 0)
+            {
+                inner_flag = 0;
+            }
+        }
+
+        if (player_score > cpu_score)
+        {
+            show_winner_score(PLAYER, &player_score);
+        }
+        else if (player_score < cpu_score)
+        {
+            show_winner_score(CPU, &cpu_score);
+        }
+        else
+        {
+            printf(NO_WINNER_GAME_MSG);
+        }
+
+        show_score(&cpu_score, &player_score);
+
+        flag = 0;
+    }
+    return 0;
+}
